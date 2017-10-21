453a7e924c21c0c95526a4d3f4b6dd64a9231142
Maria Bota <bota_maria_aurora@yahoo.com>
2017-10-21 17:00:01 +0300

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :comments
+  root to: redirect('/ideas')
+  resources :ideas
+#   # get 'pages/info'
+#   ​
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
